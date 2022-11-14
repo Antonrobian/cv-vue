4656980f06ef7f4051ba832921906c9fecafec9b
Antonrobian <antonrobian2@gmail.com>
2022-11-14 15:11:59 +0700

    component dan props

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import Header from "./components/Header.vue";
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+<template>
+    <div class="container">
+      <Header nama="Anton" role="front end developer"/>
+      <div class="about">
+        <div class="row-about">
+          <h1 class="second-title">About Me</h1>
+          <p class="second-paragraf">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            dolorem consequuntur, odit ipsam suscipit harum amet eveniet
+            voluptatum, officiis repellat sed numquam veniam maxime. Non nemo
+            placeat ut quos accusamus?
+          </p>
+        </div>
+        <div class="row-about">
+          <p>Age :</p>
+          <p>Email :</p>
+          <p>Phone :</p>
+          <p>Address :</p>
+        </div>
+        <div class="row-about">
+          <p>21</p>
+          <p>antonrobian@gmail.com</p>
+          <p>+6285234932112</p>
+          <p>kotaanyar, Probolinggo</p>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="col-contact">
+          <h1 class="third-title">Contact</h1>
+          <form>
+            <div class="form-input">
+              <input
+                class="form-control"
+                type="text"
+                name="name"
+                placeholder="Your Name"
+              />
+              <input
+                class="form-control"
+                type="email"
+                name="email"
+                placeholder="Your Email"
+              />
+            </div>
+            <textarea
+              name="text"
+              cols="61"
+              rows="10"
+              placeholder="Your Message"
+            ></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div class="col-contact">
+          <h3 class="fix">Address</h3>
+          <p class="fix">kotaanyar, Probolinggo</p>
+          <h3 class="fix">Phone</h3>
+          <p class="fix">004124115</p>
+          <h3 class="fix">Email</h3>
+          <p class="fix">antonrobian2@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      Di buat oleh <a href="https://github.com/Antonrobian">Muhammad</a>Anton Robian
+    </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    nama:{
+        type:String,
+        default: "anton",
+    },
+    role:String
+ });
+</script>
+
+<template>
+      <div class="navbar">
+        <div class="col-header">
+          <img class="images-hero" src="../assets/anton.jpg" alt="anton" />
+        </div>
+        <div class="col-header">
+          <h1>{{ nama }}</h1>
+          <p>{{ role }}</p>
+          <div class="btn-header">
+            <button type="submit">Download CV</button>
+            <button type="submit">Readme</button>
+          </div>
+        </div>
+      </div>
+</template>
